0b0c3fe16493327d535e80a5a1eb55d7bba26370
richard <nashr@vs1.rpn.me.uk>
2016-02-28 23:39:39 +0200

    update

--- SubDomainHunt.py
+++ SubDomainHunt.py
@@ -1,17 +1,17 @@
 import os, requests, sys
 from os import path
 ##credit for the code goes to https://github.com/HarryC145/
-topnumber = 3542090
+topnumber = 1100000
 ##top number as of midnight 28th feb 2015 is 3542090
 found = 0
-currentnumber = 1
+currentnumber = 1000000
 while currentnumber <= topnumber:
     checked = format(currentnumber)
     url = "http://ext-" + checked + ".livejournal.com/feed/"
     ##http://ext-NUMBER.livejournal.com/feed/
     request = requests.get(url, allow_redirects=False)
     if request.status_code == 200:
-        SitePresent = open("present.txt", "a")
+        SitePresent = open("present-1000000-1100000.txt", "a")
         SitePresent.write(url + '\n')
         SitePresent.close()
         print('I found a site, at', url)
@@ -19,7 +19,7 @@ while currentnumber <= topnumber:
         print(found)
     else:
         print('I looked for a site at', url, 'and I did not see anything')
-        notfound = open("notfound.txt", "a")
+        notfound = open("notfound-1000000-1100000.txt", "a")
         notfound.write(url + '\n')
         notfound.close()
     currentnumber = currentnumber + 1
